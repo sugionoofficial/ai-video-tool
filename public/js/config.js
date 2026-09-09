0bf20a2f31cfd82aeb00f25bbbd2f3e337b73b64
SUGIONO OFFICIAL <81317508+sugionoofficial@users.noreply.github.com>
2026-09-09 19:09:03 +0700

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,42 @@
+"use strict";
+
+window.AIVideoConfig = {
+
+  CLIENT_ID:
+    "pk_TbW0TG3A2MmE7rOM",
+
+  REDIRECT_URI:
+    "https://ai-video-tool.sugionoofficial88.workers.dev/",
+
+  TOKEN_KEY:
+    "polli_access_token",
+
+  AUTH_URL:
+    "https://enter.pollinations.ai/authorize",
+
+  TOKEN_URL:
+    "https://enter.pollinations.ai/api/oauth/token",
+
+  IMAGE_API:
+    "https://gen.pollinations.ai/image/",
+
+  VIDEO_API:
+    "https://gen.pollinations.ai/video/",
+
+  AUDIO_API:
+    "https://gen.pollinations.ai/audio/",
+
+  UPLOAD_API:
+    "https://media.pollinations.ai/upload",
+
+  VIDEO_MODEL:
+    "amazon/nova-reel-v1",
+
+  IMAGE_MODEL:
+    "black-forest-labs/flux.1-schnell"
+
+};
+
+console.log(
+  "AI Video Tool configuration loaded."
+);
