b6461847b1d566ec2ffd9f565f57669a9aaddbd1
SUGIONO OFFICIAL <81317508+sugionoofficial@users.noreply.github.com>
2026-09-12 12:24:06 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -392,61 +392,6 @@ function normalizeDuration(
  * ============================================================
  */
 
-async function reserveJob(
-  userId,
-  provider,
-  cost,
-  idempotencyKey,
-  fingerprint,
-  env
-) {
-  const res =
-    await sb(
-      "/rest/v1/rpc/start_video_job",
-      {
-        method:
-          "POST",
-
-        body:
-          JSON.stringify({
-            p_user_id:
-              userId,
-
-            p_provider:
-              provider,
-
-            p_credit_cost:
-              cost,
-
-            p_idempotency_key:
-              idempotencyKey,
-
-            p_request_fingerprint:
-              fingerprint
-          })
-      },
-      env
-    );
-
-  const data =
-    await safeJson(
-      res
-    );
-
-  if (
-    !res.ok
-  ) {
-    throw new HttpError(
-      apiError(
-        data,
-        "Credit tidak mencukupi."
-      ),
-      402
-    );
-  }
-
-  return data;
-}
 
 async function updateJob(
   jobId,
