b052c6194bd8726801eb97b06bf6cea5e6e1752b
SUGIONO OFFICIAL <81317508+sugionoofficial@users.noreply.github.com>
2026-09-12 11:42:12 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -119,44 +119,6 @@ function inferAdapter(value) {
  */
 
 /*
- * ============================================================
- * SUPABASE
- * ============================================================
- */
-
-function sbHeaders(env) {
-  return {
-    apikey:
-      env.SUPABASE_SERVICE_ROLE_KEY,
-
-    Authorization:
-      `Bearer ${env.SUPABASE_SERVICE_ROLE_KEY}`,
-
-    "Content-Type":
-      "application/json"
-  };
-}
-
-async function sb(
-  path,
-  options = {},
-  env
-) {
-  return fetch(
-    `${env.SUPABASE_URL}${path}`,
-    {
-      ...options,
-
-      headers: {
-        ...sbHeaders(
-          env
-        ),
-
-        ...(options.headers || {})
-      }
-    }
-  );
-}
 
 /*
  * ============================================================
